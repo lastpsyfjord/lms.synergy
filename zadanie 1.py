41100045d76bf43397fc65f9648f51273ff6edf5
lastpsyfjord <jenerof@yandex.ru>
2025-06-23 15:46:01 +0300

    1 задание

---
+++ zadanie 1.py
@@ -0,0 +1,5 @@
+animal_type = input("Введите вид питомца: ")
+animal_name = input("Введите кличку питомца: ")
+animal_age = int(input("Введите возраст питомца: "))
+
+print(f'Это {animal_type} по кличке "{animal_name}". Возраст: {animal_age}')
